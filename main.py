ea2cb888b3cf512c0c6a37f782816a6ebc1cab21
Lejla Hasic <hasiclejla05@gmail.com>
2026-08-04 19:21:36 +0200

    Add initial product managment functionality.

---
+++ main.py
@@ -0,0 +1,16 @@
+from product import Product
+from product_manager import ProductManager
+
+manager = ProductManager()
+
+p1 = Product("Laptop", 1500, 3)
+p2 = Product("Mouse", 25, 10)
+p3 = Product("Keyboard", 60, 5)
+
+manager.add_product(p1)
+manager.add_product(p2)
+manager.add_product(p3)
+
+manager.display_products()
+
+manager.total_inventory_value()
